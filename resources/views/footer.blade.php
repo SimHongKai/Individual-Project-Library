9d5d1a699e24e2de3d32ca2cddb5891297967a88
SimHongKai <63938461+SimHongKai@users.noreply.github.com>
2022-07-26 17:24:29 +0800

    Project Set Up
    - Main CSS
    - Several Base Layouts and Views
    - Login and Register Functionality
    - Database Connection Set Up'ed

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- footer section -->
+    <footer class="footer_section">
+        <div class="container">
+            <p>
+                <a>Some Undecided Footer Stuff here?</a>
+            </p>
+        </div>
+    </footer>
+<!-- footer section -->
---
+++ resources/views/header.blade.php
@@ -0,0 +1,57 @@
+<!-- header section strats -->
+<header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="{{route('home')}}">
+            <span>
+              Library
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link pl-lg-0" href="{{route('home')}}">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('home')}}">Books</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('home')}}">Shop</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('home')}}">Leaderboard</a>
+              </li>
+              <!-- Right Side Of Navbar -->
+              <!-- Authentication Links -->
+                        @guest
+                              <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                              </li>
+                            
+                        @else
+                              <li class="nav-item">
+                                <a class="nav-link" href="{{ route('home') }}">{{ Auth::user()->username }}</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                              </li>
+                        @endguest
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+<!-- end header section -->
